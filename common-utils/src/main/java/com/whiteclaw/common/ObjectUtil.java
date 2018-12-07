a1cf4d6506acf856b24cf02bcce9e83a2720ddec
whiteclaw <whiteclaw@yaoshi.local>
2018-12-07 17:12:32 +0800

    增加对象克隆的工具类

---
+++ common-utils/src/main/java/com/whiteclaw/common/ObjectUtil.java
@@ -0,0 +1,39 @@
+package com.whiteclaw.common;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
+import java.io.*;
+
+/**
+ * @author whiteclaw
+ * created on 2018-12-07
+ */
+public class ObjectUtil {
+
+    /**
+     * 深度克隆java对象
+     *
+     * @param obj 原对象
+     * @return 克隆对象
+     * @throws IOException            读取原对象和写入克隆对象的时候可能会发生IO异常
+     * @throws ClassNotFoundException 从流中读取克隆对象的时候, 可能发生对象的class类型找不到的异常
+     */
+
+    public static <T extends Serializable> T deepClone(T obj) throws IOException, ClassNotFoundException {
+        ByteOutputStream bos = new ByteOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(obj);
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.getBytes());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        Object cloneObj;
+        try {
+            cloneObj = ois.readObject();
+        } finally {
+            oos.close();
+            ois.close();
+        }
+        // 因为输入类型是泛型T, 所以这样做是可以的
+        @SuppressWarnings("unchecked") final T checked = (T) cloneObj;
+        return checked;
+    }
+}
